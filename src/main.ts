012f455d66e3d81a4feb6d826adab5741d9723a4
Jacob White <jake@jwhite.network>
2022-08-19 17:13:12 -0400

    Update packages

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import * as core from '@actions/core'
-import {create_config} from './config'
 import * as cloudflare from './cloudflare'
+import {create_config} from './config'
 
 export async function run(): Promise<void> {
   try {
     const config = create_config()
-    core.debug('Starting run')
     core.startGroup('Auth Check')
     await cloudflare.check_auth(config)
     core.endGroup()
     await cloudflare.purge_cache(config)
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message)
   }
 }
 
